c0524ecfea1498fa833dd8f46847d0ea2a29b3fd
olga-kacala <olga.kacala@gmail.com>
2023-12-19 18:35:22 +0100

    carusel added

--- kursy/src/components/Offer/Offer.tsx
+++ kursy/src/components/Offer/Offer.tsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
 import classes from "./Offer.module.css";
 
+export function Offer() {
+  const [activeIndex, setActiveIndex] = useState(0);
 
-export function Offer () {
+  const nextSlide = () => {
+    setActiveIndex((prevIndex) => (prevIndex + 1) % 4); 
+  };
 
-    return (
-        
-          <h1>Oferta</h1>
-       
-    )
+  useEffect(() => {
+    const intervalId = setInterval(nextSlide, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div id="carouselExample" className={classes.carousel} data-ride="carousel">
+      <div className={classes.carouselInner}>
+        {[1, 2, 3,4].map((index) => (
+          <div
+            key={index}
+            className={`${classes["carousel-item"]} ${
+              index - 1 === activeIndex ? classes.active : ""
+            }`}
+          >
+            <img
+              className="d-block w-100"
+              src={`../../Img/image${index}.jpg`}
+              alt={`Slide ${index}`}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
