0faa6269680ff78e90fee935c58170d65004f839
olga-kacala <olga.kacala@gmail.com>
2023-12-21 18:09:52 +0100

    media responsive style

--- kursy/src/components/Header/Header.tsx
+++ kursy/src/components/Header/Header.tsx
@@ -9,16 +9,16 @@ export function Header() {
           <p>T: +48 693-739-517 | E: k.krzykalski47@gmail.com</p>
         </div>
         <nav className={classes.nav}>
-          <Link className={classes.logo} to="*">
+        <Link className={classes.logo} to="*" key="logo">
             <p>KRZYKALSKI - KURSY</p>
           </Link>
-          <Link className={classes.link} to="Offer">
+          <Link className={classes.link} to="Offer" key="offer1">
             <p>Kurs dla przewodników turystycznych</p>
           </Link>
-          <Link className={classes.link} to="Offer">
+          <Link className={classes.link} to="Offer" key="offer2">
             <p>Kursy językowe</p>
           </Link>
-          <Link className={classes.link} to="Contact">
+          <Link className={classes.link} to="Contact" key="contact">
             <p>Kontakt</p>
           </Link>
         </nav>
