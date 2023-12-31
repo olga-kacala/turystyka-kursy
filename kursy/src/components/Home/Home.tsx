99cec799524cb73a404dead4badc77d19b90cfc1
olga-kacala <olga.kacala@gmail.com>
2023-12-31 13:04:15 +0100

    img path

--- kursy/src/components/Home/Home.tsx
+++ kursy/src/components/Home/Home.tsx
@@ -4,10 +4,16 @@ import { Link } from "react-router-dom";
 export function Home() {
   return (
     <div className={classes.picContainer}>
-      <img
-        src={"../../Img/lucas-albuquerque-i-p1zqFros8-unsplash.webp"}
+
+<img
+        src={process.env.PUBLIC_URL + "/Img/lucas-albuquerque-i-p1zqFros8-unsplash.webp"}
         alt="Cracow main square"
       />
+
+      {/* <img
+        src={"../../Img/lucas-albuquerque-i-p1zqFros8-unsplash.webp"}
+        alt="Cracow main square"
+      /> */}
       <div className={classes.overlayContainer}>
         <div className={classes.overlay}>
           <Link className={classes.link} to="Tour">
