c37f6a237275a5cb4ea457b539058d14fb8acfb8
olga-kacala <olga.kacala@gmail.com>
2024-01-22 13:00:36 +0100

    lang update

--- kursy/src/components/Language/Language.tsx
+++ kursy/src/components/Language/Language.tsx
@@ -6,14 +6,19 @@ export function Language() {
       <div className={classes.titleContainer}>
        
         <div className={classes.offerText}>
-          Odkryj fascynujący świat języków obcych, zgłębiając tajniki
-          rosyjskiego i niemieckiego w naszym kursie doskonalącym. Rozwijaj
-          umiejętności językowe, zdobywaj pewność siebie w komunikacji
-          międzykulturowej i otwórz nowe możliwości w karierze!
+          
+Odkryj fascynujący świat języków z naszymi różnorodnymi kursami, w tym intensywnymi kursami języka polskiego, angielskiego, niemieckiego i rosyjskiego. Specjalnie zaprojektowane kursy obejmują także doskonalenie umiejętności językowych dla przewodników po Krakowie. Zapewniamy małe grupy, wykwalifikowanych lektorów i przyjazną atmosferę. Zapisz się już dziś, aby rozwijać swoje umiejętności językowe i otworzyć nowe możliwości!
         </div>
       </div>
 
       <section className={classes.languageOffer}>
+      <article className={classes.GER}>
+          <div className={classes.marqueeText}>
+          Polish for foreigners - preparing for the respective certificate of proficiency in the Polish language as a foreign language.
+          </div>
+        </article>
+
+
         <article className={classes.GER}>
           <div className={classes.marqueeText}>
             Unser Fortgeschrittenenkurs bietet eine einzigartige Gelegenheit,
--- kursy/src/components/Tour/Tour.tsx
+++ kursy/src/components/Tour/Tour.tsx
@@ -78,17 +78,47 @@ export function Tour() {
           </ul>
 
           <h2>Wykładowcy:</h2>
-          <ul>
-            <li>Dr hab. Zygmunt Kruczek, prof. AWF Kraków.</li>
-            <li>Mgr Zuzanna Krzykalska - Przewodniczka turystyczna.</li>
-            <li>
-              Mgr Maciej Zapiór - Przewodnik po Krakowie i nauczyciel historii.
-            </li>
-            <li>
-              Dr Kazimierz Krzykalski - Neofilolog i ekspert w dziedzinie
-              kultury polskiej.
-            </li>
-          </ul>
+<ul>
+    <li>
+        <strong>Dr hab. Zygmunt Kruczek, prof. AWF Kraków</strong>
+        <ul>
+            <li>Kierownik Zakładu Geografii Turystyki i Ekologii (geografia turystyczna Polski).</li>
+            <li>Pracownik naukowo-dydaktyczny na AWF Kraków.</li>
+            <li>Absolwent odpowiedniego kierunku na Uniwersytecie Jagiellońskim.</li>
+            <li>Posiada wieloletnią praktykę jako przewodnik miejski po Krakowie.</li>
+        </ul>
+    </li>
+    <li>
+        <strong>Mgr Zuzanna Krzykalska</strong>
+        <ul>
+            <li>Absolwentka Uniwersytetu Jagiellońskiego (prawo).</li>
+            <li>Zawód przewodnika turystycznego zgodnie z obowiązującym prawodawstwem.</li>
+            <li>Posiada podstawowe prawa i obowiązki przewodnika.</li>
+            <li>Doświadczona przewodniczka z wieloletnim stażem.</li>
+        </ul>
+    </li>
+    <li>
+        <strong>Mgr Maciej Zapiór</strong>
+        <ul>
+            <li>Absolwent historii na Uniwersytecie Jagiellońskim.</li>
+            <li>Nauczyciel historii w XX LO w Krakowie.</li>
+            <li>Długoletni przewodnik po Krakowie, specjalizujący się w historii Krakowa na tle historii Polski.</li>
+        </ul>
+    </li>
+    <li>
+        <strong>Dr Kazimierz Krzykalski</strong>
+        <ul>
+            <li>Neofilolog (mgr filologii polskiej UJ 1971, mgr filologii rosyjskiej WSP Kraków 1991).</li>
+            <li>Germanista (zaliczone 10 semestrów germanistyki zaocznej na Uniwersytecie Wrocławskim 1981-1986 bez magisterium).</li>
+            <li>Mgr historii sztuki na Uniwersytecie Jagiellońskim (1978).</li>
+            <li>Dr filozofii na Uniwersytecie Jagiellońskim (2010).</li>
+            <li>Prowadzi wykłady teoretyczne m.in. z dziejów kultury polskiej, sztuki Krakowa, literatury polskiej, Krakowa współczesnego, topografii Krakowa i strefy podmiejskiej.</li>
+            <li>Specjalista w zakresie wykładów metodyczno-zawodowych, metodyki oprowadzania wycieczek miejskich, specyfiki wycieczek szkolnych, wycieczek osób niepełnosprawnych, szkoleń terenowych (trasy miejskie) jako instruktor szkoleń terenowych.</li>
+            <li>Prowadzi szkolenia muzealne, a także współpracuje z pracownikami działów oświatowych muzeów, często z ich kierownikami.</li>
+        </ul>
+    </li>
+</ul>
+
 
           <h2>Warunki Uczestnictwa:</h2>
           <ul>
@@ -122,11 +152,14 @@ export function Tour() {
 
           <h2>Zapisy:</h2>
           <ul>
-    <li>Osobiście: w biurze firmy (pn.-pt. 10-18) Kraków, ul. Zachodnia 10/50</li>
+    <li>Osobiście: w biurze firmy (pn.-pt. 10-18) Kraków, ul. Zachodnia 10/50. Dostarczenie wypełnionej <a href={
+          process.env.PUBLIC_URL +
+          "/Img/ankietaPrzewodnik.pdf"
+        } download="ankietaPrzewodnik.pdf">ankiety</a> wpisowej + kserokopia świadectwa dojrzałości + 2 fotografie  </li>
     <li>Email: wypełnioną <a href={
           process.env.PUBLIC_URL +
           "/Img/ankietaPrzewodnik.pdf"
-        } download="ankietaPrzewodnik.pdf">ankietę</a> prześlij na k.krzykalski47@gmail.com </li>
+        } download="ankietaPrzewodnik.pdf">ankietę</a> prześlij na k.krzykalski47@gmail.com. Pozostałe dokumenty można dostarczyć podczas kursu (kserokopia świadectwa dojrzałości + 2 fotografie)</li>
 </ul>
           <p>Jeśli jesteś bliżej zainteresowany szczegółami kursu, serdecznie
             zapraszamy do naszego biura w godzinach otwarcia, aby otrzymać pełną
