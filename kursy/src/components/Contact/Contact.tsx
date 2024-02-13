ab8e9552bbb70254a2116bad064d4ff8c60a53f8
olga-kacala <olga.kacala@gmail.com>
2024-02-13 17:47:14 +0100

    maps

--- kursy/src/components/Contact/Contact.tsx
+++ kursy/src/components/Contact/Contact.tsx
@@ -27,6 +27,24 @@ export function Contact() {
           <p> Kazimierz Krzykalski Wycieczki Lokalne i Kursy</p>
           <p>NIP: 9441590188</p>
         </div>
+
+        <div className={classes.mapsComponent}>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2563.2803977088665!2d19.904233177317654!3d50.024841517916286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47165d8ed9bb0f57%3A0xb74abcf315a17048!2sKazimierz%20Krzykalski%20Wycieczki%20i%20Kursy!5e0!3m2!1spl!2spl!4v1707838625887!5m2!1spl!2spl"
+            width="500px"
+            height="450px"
+            style={{ border: "0", marginBottom: "10vh", padding: "2vh" }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+          <img
+            className={classes.qrCode}
+            title="Scan me"
+            alt="QR code"
+            src={process.env.PUBLIC_URL + "/Img/qr.png"}
+          />
+        </div>
       </div>
     </div>
   );
